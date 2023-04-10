96c6bde173ef89523e5491d3108f357d16d2279f
dgengtek <dgengtek@users.noreply.github.com>
2023-04-10 20:17:26 +0200

    Fix qemu convert path

--- scripts/qemu/convert_raw.sh
+++ scripts/qemu/convert_raw.sh
@@ -4,5 +4,6 @@ set -eux
 readonly build_directory="${1:?Build directory not supplied}"
 readonly name="${2:?Name not supplied}"
 
-cd "${build_directory}/${name}-qemu"
+pwd
+cd "${build_directory}"
 qemu-img convert -O raw "${name}.qcow2" "${name}.raw"
