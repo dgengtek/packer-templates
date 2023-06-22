1ce7af6a86c6015be1f4fdd28f4a2875753b050c
dgengtek <dgengtek@users.noreply.github.com>
2023-06-22 22:07:43 +0200

    Run saltstack inside a python venv

--- scripts/debian/install_salt.sh
+++ scripts/debian/install_salt.sh
@@ -4,5 +4,4 @@ set -ex
 [ -n "$SALT_VERSION_TAG" ]
 
 apt-get update
-apt-get install -y curl python3-apt build-essential
-apt-get install --reinstall -y build-essential
+apt-get install -y curl python3 python3-apt python3-venv build-essential
--- scripts/install_salt.sh
+++ scripts/install_salt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -ex
 
 [ -n "$SALT_VERSION_TAG" ]
@@ -6,8 +6,12 @@ set -ex
 
 curl -o bootstrap-salt.sh -L https://bootstrap.saltproject.io
 
-systemctl mask salt-minion
-env -i PATH=$PATH bash bootstrap-salt.sh \
+systemctl mask salt-minion.service
+
+python3 -m venv /root/venv
+source /root/venv/bin/activate
+pip install pyyaml
+bash bootstrap-salt.sh \
   -dX \
   -x python3 \
   -g "$SALT_GIT_URL" \
@@ -17,7 +21,10 @@ env -i PATH=$PATH bash bootstrap-salt.sh \
   -P \
   git ${SALT_VERSION_TAG}
 
-systemctl unmask salt-minion
-systemctl stop salt-minion || true
-systemctl enable salt-minion
+systemctl unmask salt-minion.service
+# redirecting creates a mask? need to copy in two steps
+systemctl cat salt-minion.service | sed 's,^ExecStart=.*,ExecStart=/root/venv/bin/salt-minion,' > salt-minion.service
+cp -fv salt-minion.service /etc/systemd/system/
+rm salt-minion.service
+systemctl enable salt-minion.service
 rm -f /etc/salt/minion_id
