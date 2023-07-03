b89d74785faa866b4671411c02555f8ba36c1ca4
dgengtek <dgengtek@users.noreply.github.com>
2023-07-03 19:57:26 +0200

    Fix saltstack lsb_distrib_id for debian

--- scripts/debian/install_salt.sh
+++ scripts/debian/install_salt.sh
@@ -5,3 +5,7 @@ set -ex
 
 apt-get update
 apt-get install -y curl python3 python3-apt python3-venv build-essential
+
+# fix for debian with no lsb release in venv
+mkdir -p /etc/salt
+echo "lsb_distrib_id: Debian" >> /etc/salt/grains
