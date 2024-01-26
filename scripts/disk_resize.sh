9bd24cc93989a911d525609606ed62c25aac5dc5
dgengtek <dgengtek@users.noreply.github.com>
2024-01-26 08:59:54 +0100

    Add missing device arguments

--- scripts/disk_resize.sh
+++ scripts/disk_resize.sh
@@ -4,9 +4,10 @@ set -exu
 [[ -n "$DEV_DISK" ]]
 [[ -n "$DEV_PARTITION_NR" ]]
 
-sudo sgdisk --move-second-header
+sudo sgdisk --move-second-header "$DEV_DISK"
+sudo partprobe "$DEV_DISK"
 sudo sgdisk --delete=${DEV_PARTITION_NR} "$DEV_DISK"
-sudo sgdisk --new=${DEV_PARTITION_NR}:0:0 --typecode=0:8e00 --change-name=0:pv ${DEV_DISK}
+sudo sgdisk --new=${DEV_PARTITION_NR}:0:0 --typecode=${DEV_PARTITION_NR}:8e00 --change-name=${DEV_PARTITION_NR}:pv ${DEV_DISK}
 sudo partprobe "$DEV_DISK"
 
 sudo pvresize ${DEV_DISK}${DEV_PARTITION_NR}
